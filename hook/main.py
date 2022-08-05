09fc0af30f69d3945902a8e6e4441555c5503bea
Pavel Ohursov <cucumb3rpasha@gmail.com>
2022-08-05 22:00:49 +0300

    update services in parallel

--- hook/main.py
+++ hook/main.py
@@ -5,14 +5,15 @@ from flask import Flask, Response, request
 app = Flask(__name__)
 
 @app.route('/hook/push',methods=['POST'])
-def push_hook():
-    print(request.data)
+async def push_hook():
+    await update_services()
 
+    return Response(status=200)
+
+async def update_services():
     git.cmd.Git().fetch('https://github.com/l0s0s/server-config','master')
     git.cmd.Git().pull('https://github.com/l0s0s/server-config','master')
     os.system('docker-compose up -d --force-recreate')
-
-    return Response(status=200)
 
 if __name__ == '__main__':
     app.run(port=8080)
